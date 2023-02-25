1b366bf1dd21d32fcd613ce2b12328f636bce2c9
Bibek Rajbhandari <bib.rajbhandari@gmail.com>
2023-02-25 16:20:45 +0800

    login code cleanup

--- frontend/src/Components/RouteProvider.jsx
+++ frontend/src/Components/RouteProvider.jsx
@@ -3,18 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import SignUp from "../Pages/Signup/Signup";
 import ProfileSetup from "../Pages/Signup/ProfileSetup";
-import CompanySignUp from "../Pages/Signup/CompanySignup";
 import Login from "../Pages/Login/Login";
-import LoginComp from "./Login/LoginComp";
 
 export default function RouteProvider() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
-      <Route path="/" element={<LoginComp name="signup" />} />
-
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/company/signup" element={<CompanySignUp />} />
       <Route path="/setprofile" element={<ProfileSetup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
